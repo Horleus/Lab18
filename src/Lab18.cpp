a5f9319e5d48a055906286ab83d82ad3b25c50b0
win <win@WINPC>
2020-05-22 01:59:36 +0300

    Ready

--- src/Lab18.cpp
+++ src/Lab18.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
+const int N = 10;
+const int M = 20;
+char mas[N][M];
+
+char line[N*M];
+
 int main() {
+	int i;
+	setlocale(LC_ALL, "Ukrainian");
+	for (i = 0; i < N; i++)
+	{
+		cout << "Введiть " << i+1 << " рядок: ";
+		cin.getline(mas[i], M, '\n');
+	}
+
+	strcpy(line, mas[0]);
+	for (i = 1; i < N; i++)
+		strcat(line, mas[i]);
+
+	cout << "Об'єднаний рядок:" << endl;
+	cout << line <<endl;
 	return 0;
 }
